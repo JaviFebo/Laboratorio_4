a9b2aa335e5767380c9f58559620258152ac68f4
JaviFebo <javi.bolanos.c@gmail.com>
2025-10-17 22:32:02 -0600

    Inicialización de matriz y cálculo de su cantidad de elementos.

---
+++ ejercicio1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+        int matriz [3][3] = {
+                {4, 1, 8},
+                {2, 6, 3},
+                {9, 7, 5}
+        };
+	int elementos = sizeof(matriz) / sizeof(matriz [0][0]);
+}
