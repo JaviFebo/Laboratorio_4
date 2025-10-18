ffaea53afb0ea47049078478d6610cdf1a35f101
JaviFebo <javi.bolanos.c@gmail.com>
2025-10-17 22:51:23 -0600

    Función para convertir la matriz en un arreglo unidimensional implementada.

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+void arr_uni (int *ptr, int *ptr_arr, int elementos) {
+
+	for (int i = 0; i < elementos; i++) {
+		*(ptr_arr + i) = *(ptr + i);
+	}
+}
+
 int main () {
         int matriz [3][3] = {
                 {4, 1, 8},
                 {2, 6, 3},
                 {9, 7, 5}
         };
-	int elementos = sizeof(matriz) / sizeof(matriz [0][0]);
+	int elementos = sizeof (matriz) / sizeof (matriz [0][0]);
+	int arreglo [elementos];
+	
+	arr_uni (&matriz [0][0], &arreglo [0], elementos);
 }
