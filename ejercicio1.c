d78303a85c119117c5fca4eff008968f41ca6120
JaviFebo <javi.bolanos.c@gmail.com>
2025-10-18 00:11:28 -0600

    Añadida función de cambio de variable, función de ordenado de un arreglo y función para imprimir un arreglo

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-void arr_uni (int *ptr, int *ptr_arr, int elementos) {
+void cambiar (int *a, int *b) {
+	int temp = *b;
+	*b = *a;
+	*a = temp;
+}
+
+void imprimir_arr (int *arreglo, int elementos) {
+	for (int i = 0; i < elementos; i++) {
+		printf("%d ", *(arreglo + i));
+	}
+    	printf ("\n");
+}
+
+void ordenar (int *ptr, int elementos) {
+        for (int i = 0; i < elementos; i++) {
+                for (int j = 0; j < elementos; j++) {
+                        if (*(ptr + j) > *(ptr + i)) {
+                                cambiar (ptr + j, ptr + i);
+                        }
+                }
+        }
+}
+
+
+void mat_a_arr (int *ptr, int *ptr_arr, int elementos) {
 
 	for (int i = 0; i < elementos; i++) {
 		*(ptr_arr + i) = *(ptr + i);
@@ -9,12 +33,16 @@ void arr_uni (int *ptr, int *ptr_arr, int elementos) {
 
 int main () {
         int matriz [3][3] = {
-                {4, 1, 8},
-                {2, 6, 3},
-                {9, 7, 5}
+                {4, 15, 9},
+                {7, 6, 1},
+                {12, 6, 3}
         };
-	int elementos = sizeof (matriz) / sizeof (matriz [0][0]);
-	int arreglo [elementos];
+	int elementos_matriz = sizeof (matriz) / sizeof (matriz [0][0]);
+	int arreglo_matriz [elementos_matriz];
 	
-	arr_uni (&matriz [0][0], &arreglo [0], elementos);
+	mat_a_arr (&matriz [0][0], &arreglo_matriz [0], elementos_matriz);
+	imprimir_arr (&arreglo_matriz [0], elementos_matriz);
+	
+	ordenar (&arreglo_matriz [0], elementos_matriz);
+	imprimir_arr (&arreglo_matriz [0], elementos_matriz);
 }
