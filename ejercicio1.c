569a07d64aac1998062042b9db6ef5ff45d90aab
JaviFebo <javi.bolanos.c@gmail.com>
2025-10-18 12:45:08 -0600

    Añadida función para convertir de arreglo a matriz, función para imprimir matriz y formateo de salidas.

--- ejercicio1.c
+++ ejercicio1.c
@@ -6,11 +6,20 @@ void cambiar (int *a, int *b) {
 	*a = temp;
 }
 
-void imprimir_arr (int *arreglo, int elementos) {
+void imprimir_arr (int *ptr_arr, int elementos) {
 	for (int i = 0; i < elementos; i++) {
-		printf("%d ", *(arreglo + i));
+		printf("%d ", *(ptr_arr + i));
 	}
     	printf ("\n");
+}
+
+void imprimir_mat (int *ptr_mat, int filas, int columnas) {
+	for (int i = 0; i < filas; i++) {
+		for (int j = 0; j < columnas; j++) {
+			printf ("%d ", *(ptr_mat + j + i * columnas));
+		}
+		printf ("\n");
+	}
 }
 
 void ordenar (int *ptr, int elementos) {
@@ -24,10 +33,15 @@ void ordenar (int *ptr, int elementos) {
 }
 
 
-void mat_a_arr (int *ptr, int *ptr_arr, int elementos) {
-
+void mat_a_arr (int *ptr_mat, int *ptr_arr, int elementos) {
 	for (int i = 0; i < elementos; i++) {
-		*(ptr_arr + i) = *(ptr + i);
+		*(ptr_arr + i) = *(ptr_mat + i);
+	}
+}
+
+void arr_a_mat (int *ptr_mat, int *ptr_arr, int elementos) {
+	for (int i = 0; i < elementos; i++) {
+		*(ptr_mat + i) = *(ptr_arr + i);
 	}
 }
 
@@ -38,11 +52,22 @@ int main () {
                 {12, 6, 3}
         };
 	int elementos_matriz = sizeof (matriz) / sizeof (matriz [0][0]);
+	int filas_matriz = sizeof (matriz) / sizeof (matriz [0]);
+	int columnas_matriz = sizeof (matriz [0]) / sizeof (matriz [0][0]);
 	int arreglo_matriz [elementos_matriz];
 	
+	printf ("Matriz original: \n");	
+	imprimir_mat (&matriz [0][0], filas_matriz, columnas_matriz);
+	
+	printf ("\nArreglo unidimensional: \n");
 	mat_a_arr (&matriz [0][0], &arreglo_matriz [0], elementos_matriz);
 	imprimir_arr (&arreglo_matriz [0], elementos_matriz);
 	
+	printf ("\nArreglo unidimensional ordenado: \n");
 	ordenar (&arreglo_matriz [0], elementos_matriz);
 	imprimir_arr (&arreglo_matriz [0], elementos_matriz);
+	
+	printf ("\nMatriz ordenada: \n");
+	arr_a_mat (&matriz [0][0], &arreglo_matriz [0], elementos_matriz);
+	imprimir_mat (&matriz [0][0], filas_matriz, columnas_matriz);
 }
