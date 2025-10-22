463d44bc8ac2e930cda6890224a1dcea36c56ce3
JaviFebo <javi.bolanos.c@gmail.com>
2025-10-21 23:36:54 -0600

    Añadida documentación

--- ejercicio2.c
+++ ejercicio2.c
@@ -7,21 +7,21 @@ int posicion_signo (char *palabra) {
 
 	for (int i = 0; i < longitud_str; i++) {
 		if (ispunct (palabra [i])) {
-			return i;
+			return i; //Si el caracter es un signo de puntuación devuelve la posición del caracter
 		}
 	}	
-	return -1;
+	return -1; //Si no hay signos de puntuación devuelve -1
 }
 
 void quitar_signo (char *palabra, int posicion) {
 	int longitud_str = strlen (palabra);
 	
 	if (posicion == -1) {
-		return;
+		return; //Si la posición es -1 sale de la función
 	}
 	
 	for (int i = posicion; i < longitud_str; i++) {
-		palabra [i] = palabra [i + 1];
+		palabra [i] = palabra [i + 1]; //Mueve los caractéres hacia la izquierda en la posición del signo de puntuación
 	}
 }
 
@@ -29,11 +29,11 @@ void agregar_espacio_blanco (FILE *archivo_entrada, FILE *archivo_salida) {
 	int c = fgetc (archivo_entrada);
 	
 	if (c == '\n') {
-		fputc ('\n', archivo_salida);
+		fputc ('\n', archivo_salida); //Si lee un salto de línea en la entrada lo pone en la salida
 	}
 	
 	else if (c == ' ') {
-		fputc (' ', archivo_salida);
+		fputc (' ', archivo_salida); //Si lee un espacio en la entrada lo pone en la salida
 	}
 	
 	if (c != EOF && c != '\n' && c != ' ') {
@@ -54,18 +54,18 @@ void reemplazar_palabra (char *nombre_archivo, char *palabra_buscar, char *palab
 	int contador = 0;
 
 	while (fscanf (archivo_entrada, "%99s", palabra) == 1) {
-		int posicion = posicion_signo(palabra);
+		int posicion = posicion_signo(palabra); 
 		
-		strcpy (palabra_limpia, palabra);
-		quitar_signo (palabra_limpia, posicion);
+		strcpy (palabra_limpia, palabra); 
+		quitar_signo (palabra_limpia, posicion); 
 		
 		if (strcmp (palabra_limpia, palabra_buscar) == 0) {
-			fputs (palabra_reemplazo, archivo_salida);
+			fputs (palabra_reemplazo, archivo_salida); //Si la palabra sin signos es igual a la buscada de agrega el reemplazo en la salida
 			contador++;
 		}
 		
 		else {
-			fputs (palabra, archivo_salida);
+			fputs (palabra, archivo_salida); //Si la palabra sin signos no es igual a la buscada se pasa directamente a la salida
 		}
 		
 		agregar_espacio_blanco (archivo_entrada, archivo_salida);
